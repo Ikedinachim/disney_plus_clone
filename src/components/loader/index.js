874ce7e623f04351f1b9858e804c9c5e9857ae98
xyborgg <djceeno@gmail.com>
2021-11-28 22:54:15 +0100

    Icons Fixed

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './loader.module.scss'
-import LoadingGIF from '../../assets/img/loading.gif'
+import LoadingGIF from '../../assets/img/loader.gif'
 
 
 const Loader = () => {
