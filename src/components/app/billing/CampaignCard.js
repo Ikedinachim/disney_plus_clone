967dd9ddd0a937841c6211a41326ac685eda38e9
oluwoleadetifa <adetifaoluwole@gmail.com>
2022-01-12 11:19:40 +0100

    Campaign card appearance change

--- src/components/app/billing/CampaignCard.js
+++ src/components/app/billing/CampaignCard.js
@@ -4,21 +4,17 @@ import NumberFormat from "react-number-format";
 const CampaignCard = ({ campaign }) => {
   return (
     <Fragment>
-      <div className="card tnx-card rounded bd-0 shadow-sm">
-        <div className="card-body pd-y-5">
-          <tr>
-            <td>{campaign.channel}</td>
-            <td className="tx-right">
-              <NumberFormat
-                value={campaign.price}
-                displayType={"text"}
-                thousandSeparator={true}
-                prefix={"₦"}
-              />
-            </td>
-          </tr>
-        </div>
-      </div>
+      <tr>
+        <td>{campaign.channel}</td>
+        <td className="tx-right">
+          <NumberFormat
+            value={campaign.price}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"₦"}
+          />
+        </td>
+      </tr>
     </Fragment>
   );
 };
