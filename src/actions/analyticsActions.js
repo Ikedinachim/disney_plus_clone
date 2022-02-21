25b2ea822d88b836dc974092c31e395210075dcb
oluwoleadetifa <adetifaoluwole@gmail.com>
2022-02-21 20:52:38 +0100

    analytics things

--- src/actions/analyticsActions.js
+++ src/actions/analyticsActions.js
@@ -4,6 +4,9 @@ import {
   PROPELLER_CAMPAIGN_REQUEST,
   PROPELLER_CAMPAIGN_SUCCESS,
   PROPELLER_CAMPAIGN_FAIL,
+  PROPELLER_OS_CAMPAIGN_REQUEST,
+  PROPELLER_OS_CAMPAIGN_SUCCESS,
+  PROPELLER_OS_CAMPAIGN_FAIL,
   CLEAR_ERRORS,
 } from "../constants/analyticsConstants";
 
@@ -12,19 +15,20 @@ const baseURL = "https://ssp-api.propellerads.com/v5/";
 const axios = Axios.create({
   baseURL,
 });
+const token = "8fcd81e29610f456f221b5727fdac71a116b1a45fcde6793";
 
 //Get Statistics data
 export const getPropellerCampaign = (propellerId) => async (dispatch) => {
   try {
     dispatch({ type: PROPELLER_CAMPAIGN_REQUEST });
-    const token = "8fcd81e29610f456f221b5727fdac71a116b1a45fcde6793";
 
     const config = {
       headers: {
+        accept: "application/json",
         "Content-type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-      Request_body: {
+      data: {
         group_by: "campaign_id",
         day_from: "2022-01-01 00:00:00",
         day_to: "2022-02-22 23:59:59",
@@ -34,8 +38,8 @@ export const getPropellerCampaign = (propellerId) => async (dispatch) => {
       },
     };
 
-    const { data } = await axios.get("/adv/statistics", config);
-    if (data.statusCode === 200) {
+    const { data } = await axios.post("/adv/statistics", config);
+    if (data.status === "success") {
       dispatch({
         type: PROPELLER_CAMPAIGN_SUCCESS,
         payload: data.data,
@@ -49,6 +53,48 @@ export const getPropellerCampaign = (propellerId) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PROPELLER_CAMPAIGN_FAIL,
+      payload: error.message,
+    });
+  }
+};
+
+//Get Statistics data by os
+export const getPropellerOsCampaign = (propellerId) => async (dispatch) => {
+  try {
+    dispatch({ type: PROPELLER_OS_CAMPAIGN_REQUEST });
+
+    const config = {
+      headers: {
+        accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const body = {
+      group_by: "os",
+      day_from: "2022-01-01 00:00:00",
+      day_to: "2022-02-22 23:59:59",
+      campaign_id: [propellerId],
+      geo: ["NG"],
+      dept: ["nativeads"],
+    };
+
+    const { data } = await axios.post("/adv/statistics", body, config);
+    if (data.status === "success") {
+      dispatch({
+        type: PROPELLER_OS_CAMPAIGN_SUCCESS,
+        payload: data.data,
+      });
+    } else {
+      dispatch({
+        type: PROPELLER_OS_CAMPAIGN_FAIL,
+        payload: data.message,
+      });
+    }
+  } catch (error) {
+    dispatch({
+      type: PROPELLER_OS_CAMPAIGN_FAIL,
       payload: error.message,
     });
   }
--- src/reducers/anayticsReducers.js
+++ src/reducers/anayticsReducers.js
@@ -2,9 +2,16 @@ import {
   PROPELLER_CAMPAIGN_REQUEST,
   PROPELLER_CAMPAIGN_SUCCESS,
   PROPELLER_CAMPAIGN_FAIL,
+  PROPELLER_OS_CAMPAIGN_REQUEST,
+  PROPELLER_OS_CAMPAIGN_SUCCESS,
+  PROPELLER_OS_CAMPAIGN_FAIL,
+  PROPELLER_MOBILE_REQUEST,
+  PROPELLER_MOBILE_SUCCESS,
+  PROPELLER_MOBILE_FAIL,
   CLEAR_ERRORS,
 } from "../constants/analyticsConstants";
 
+/////Statistics details (impression, click, conversions)/////
 export const getPropellerCampaignsReducer = (
   state = { propellerCampaigns: [] },
   action
@@ -22,6 +29,71 @@ export const getPropellerCampaignsReducer = (
       };
 
     case PROPELLER_CAMPAIGN_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+/////OS Campaign details////////
+export const getOsCampaignsReducer = (state = { OsCampaigns: [] }, action) => {
+  switch (action.type) {
+    case PROPELLER_OS_CAMPAIGN_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case PROPELLER_OS_CAMPAIGN_SUCCESS:
+      return {
+        loading: false,
+        OsCampaigns: action.payload,
+      };
+
+    case PROPELLER_OS_CAMPAIGN_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+////Mobile Campaign details////
+export const getMobileCampaignsReducer = (
+  state = { mobileCampaigns: [] },
+  action
+) => {
+  switch (action.type) {
+    case PROPELLER_MOBILE_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case PROPELLER_MOBILE_SUCCESS:
+      return {
+        loading: false,
+        mobileCampaigns: action.payload,
+      };
+
+    case PROPELLER_MOBILE_FAIL:
       return {
         loading: false,
         error: action.payload,
